f5c0e2f25c4ebceb071458ae4bd95ed80369eebc
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-09-23 14:38:16 +0200

    Test product loading and routes

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,17 @@
+import { Col, Row } from "react-bootstrap"
+import { StoreProduct } from "../components/StoreProduct"
+import storeProducts from "../data/products.json"
+
 export function Store () {
     return (
-        <h1>Store</h1>
+        <><h1>Store</h1>
+        <Row md={2} xs={1} lg={3} className="g-3">
+            {storeProducts.map(item =>(
+                <Col key={item.id}>
+                <StoreProduct {...item} />
+                </Col>
+            ))}
+        </Row>
+        </>
     )
 }
