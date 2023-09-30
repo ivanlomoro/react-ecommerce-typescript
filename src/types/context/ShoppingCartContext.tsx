27d26aaf070beca3c64da36d26a3df635143ad6e
Ivan Martin Lomoro <ivanmartin_95@hotmail.com>
2023-09-30 12:22:37 +0200

    Added shopping cart and a littles changes

--- src/types/context/ShoppingCartContext.tsx
+++ src/types/context/ShoppingCartContext.tsx
@@ -1,94 +1,107 @@
-import {createContext, useContext, useState } from "react";
+import { createContext, useContext, useReducer } from "react";
 import { ShoppingCart } from "../../components/ShoppingCart";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
-import { CartProduct, ShoppingCartProviderProps} from "../product";
+import { CartProduct, ShoppingCartProviderProps } from "../product";
 
+type ShoppingCartContextType = {
+    openCart: () => void;
+    closeCart: () => void;
+    getProductQuantity: (id: number) => number;
+    increaseCartQuantity: (id: number) => void;
+    decreaseCartQuantity: (id: number) => void;
+    removeFromCart: (id: number) => void;
+    cartQuantity: number;
+    cartProducts: CartProduct[];
+};
 
-type ShoppingCartContext={
-    openCart: () => void
-    closeCart: () => void
-    getProductQuantity: (id: number) => number
-    increaseCartQuantity: (id: number) => void
-    decreaseCartQuantity: (id: number) => void
-    removeFromCart: (id: number) => void
-    cartQuantity:number
-    cartProducts: CartProduct[]
-}
+type Action =
+    | { type: "ADD_TO_CART"; id: number }
+    | { type: "REMOVE_FROM_CART"; id: number };
 
-const ShoppingCartContext = createContext({} as ShoppingCartContext);
+const initialState: CartProduct[] = [];
+
+const shoppingCartReducer = (
+    state: CartProduct[],
+    action: Action
+): CartProduct[] => {
+    let existingProduct;
+
+    switch (action.type) {
+        case "ADD_TO_CART":
+            existingProduct = state.find((item) => item.id === action.id);
+            if (existingProduct) {
+                return state.map((item) =>
+                    item.id === action.id
+                        ? { ...item, quantity: item.quantity + 1 }
+                        : item
+                );
+            } else {
+                return [...state, { id: action.id, quantity: 1 }];
+            }
+        case "REMOVE_FROM_CART":
+            return state.filter((item) => item.id !== action.id);
+        default:
+            return state;
+    }
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContextType);
 
 export function useShoppingCart() {
-    return useContext(ShoppingCartContext)
+    return useContext(ShoppingCartContext);
 }
 
-export function ShoppingCartProvider({ children } : ShoppingCartProviderProps){
-    const [isOpen, setIsOpen] = useState(false)
-    const [cartProducts, setCartProducts] = useLocalStorage<CartProduct[]>("shopping-cart",[])
-    
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+    const [isOpen, setIsOpen] = useLocalStorage<boolean>("cartIsOpen", false);
+    const [cartProducts, dispatch] = useReducer(
+        shoppingCartReducer,
+        initialState
+    );
+
     const cartQuantity = cartProducts.reduce(
         (quantity, item) => item.quantity + quantity,
         0
-    )
+    );
 
-    const openCart = () => setIsOpen(true)
-    const closeCart = () => setIsOpen(false)
+    const openCart = () => setIsOpen(true);
+    const closeCart = () => setIsOpen(false);
 
-    function getProductQuantity(id:number){
-        return cartProducts.find(item => item.id === id)?.quantity || 0
+    function getProductQuantity(id: number) {
+        return cartProducts.find((item) => item.id === id)?.quantity || 0;
     }
 
-    function increaseCartQuantity(id: number){
-        setCartProducts(currItems =>{
-            if(currItems.find(item => item.id === id) == null){
-                return [...currItems, {id, quantity: 1}]
-            }else {
-                return currItems.map(item => {
-                    if(item.id === id){
-                        return {...item, quantity: item.quantity+1}
-                    }else{
-                        return item
-                    }
-                })
-
-            }
-        })
+    function increaseCartQuantity(id: number) {
+        dispatch({ type: "ADD_TO_CART", id });
     }
 
-    function decreaseCartQuantity(id: number){
-        setCartProducts(currItems =>{
-            if(currItems.find(item => item.id === id)?.quantity === 1 ){
-                return currItems.filter(item => item.id !== id)
-            }else {
-                return currItems.map(item => {
-                    if(item.id === id){
-                        return {...item, quantity: item.quantity-1}
-                    }else{
-                        return item
-                    }
-                })
-
-            }
-        })
+    function decreaseCartQuantity(id: number) {
+        const existingProduct = cartProducts.find((item) => item.id === id);
+        if (existingProduct && existingProduct.quantity === 1) {
+            removeFromCart(id);
+        } else {
+            dispatch({ type: "ADD_TO_CART", id });
+        }
     }
 
-    function removeFromCart(id:number){
-        setCartProducts(currItems =>{
-            return currItems.filter(item => item.id !== id)
-        } )
+    function removeFromCart(id: number) {
+        dispatch({ type: "REMOVE_FROM_CART", id });
     }
 
     return (
-        <ShoppingCartContext.Provider value={{
-            getProductQuantity,
-            increaseCartQuantity,
-            decreaseCartQuantity,
-            removeFromCart,
-            openCart,
-            closeCart,
-            cartProducts,
-            cartQuantity}}>
+        <ShoppingCartContext.Provider
+            value={{
+                getProductQuantity,
+                increaseCartQuantity,
+                decreaseCartQuantity,
+                removeFromCart,
+                openCart,
+                closeCart,
+                cartProducts,
+                cartQuantity,
+            }}
+        >
             {children}
-            <ShoppingCart isOpen={isOpen}/>
+            <ShoppingCart isOpen={isOpen} />
         </ShoppingCartContext.Provider>
-    )
+    );
 }
